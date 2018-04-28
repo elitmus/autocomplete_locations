f44f2c8d818457a0cdac062fb9eacee1939e09f5
C Rahul <crahul@elitmus.com>
2018-04-29 03:25:25 +0530

    [UPGRADE] Now search for near locations on this table

--- app/models/autocomplete_locations/autocomplete_location.rb
+++ app/models/autocomplete_locations/autocomplete_location.rb
@@ -1,6 +1,8 @@
 module AutocompleteLocations
   class AutocompleteLocation < ActiveRecord::Base
 
+    reverse_geocoded_by :latitude, :longitude
+
     self.table_name = 'autocomplete_locations'
 
     scope :at_lat_long, -> (lat, long) { where(["abs(autocomplete_locations.latitude-#{lat}) <= 1e-4 AND abs(autocomplete_locations.longitude-#{long}) <= 1e-4"]) }
