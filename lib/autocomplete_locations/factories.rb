a022f384ed80e6e3199bbee103e512df696788c4
Shubham Pandey <shubham.pandey@elitmus.com>
2018-10-09 12:50:38 +0530

    [FIX]Factories updated removed deprecated warnings

--- lib/autocomplete_locations/factories.rb
+++ lib/autocomplete_locations/factories.rb
@@ -3,11 +3,11 @@ require_relative '../../app/models/autocomplete_locations/autocomplete_location'
 FactoryBot.define do
   factory :autocomplete_locations, :class => AutocompleteLocations::AutocompleteLocation do
     sequence(:id) {|n| n}
-    state 15
-    city_id 1
-    city "Bangalore"
-    latitude 12.9716
-    longitude 77.5946
-    default_city true
+    state { 15 }
+    city_id { 1 }
+    city { "Bangalore" }
+    latitude { 12.9716 }
+    longitude { 77.5946 }
+    default_city { true }
   end
 end
